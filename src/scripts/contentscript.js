fb999e4f0fe3c9de3500cc7b4ffff3ce952bd84e
Matt Smith <hellboy1975@gmail.com>
2011-12-10 21:51:53 +1030

    [no] changed to gay
    Toolshed link turned to Fourtheye

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -3,11 +3,26 @@ function removeFuckingTriangle() {
 	$('img.inlineimg').hide();
 }
 
+// changes all of those pesky Toolshed references to a real website
+function addFourtheye() {
+	$('a[href="http://toolshed.down.net/"]').attr('href', 'http://fourtheye.net').html("Fourtheye");
+}
+
+function restoreGays() {
+	html = $("#brotablebeta").html();
+	rep = html.replace(/\[no\]/g, "gay");
+	$("#brotablebeta").html(rep);
+}
+
 function pageload()
 {
 	if (location.href.indexOf('toolnavy.com') > -1) {
 		//chrome.pageAction.show(tabId);
 		removeFuckingTriangle();
+		
+		addFourtheye();
+		
+		restoreGays();
 	}
 	//else chrome.pageAction.hide(tabId);  
 }
